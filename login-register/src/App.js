11e806218251ae7d676b4333b798f7dd519edb89
dineshroyal04 <dinesh.18mis7181@vitap.ac.in>
2023-12-10 05:57:04 +0530

    second commit

--- login-register/src/App.js
+++ login-register/src/App.js
@@ -1,6 +1,15 @@
+import "./App.css";
+import Login from "./components/login/Login.js";
+import Register from "./components/register/Register.js";
+import HomePage from "./components/homepage/HomePage.js";
+
 function App(){
   return (
-        <div><h1>dinesh</h1></div>
+        <div>
+          <Login />
+          <Register />
+          <HomePage />
+        </div>
   );
 }
 export default App;
